f5596c8432e2bc014d5cf87473ddcb85708b61b2
FhRh <yiyop@naver.com>
2024-06-14 01:18:26 +0900

    signup만들다가 브랜치 파러가는중

---
+++ gps/src/payload/request/UserSignupReqeust.java
@@ -0,0 +1,10 @@
+package payload.request;
+
+public class UserSignupReqeust {
+    String email;
+    String phone;
+    String nickname;
+    String password;
+    boolean terms;
+    boolean subscribe;
+}
