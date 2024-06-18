4e3863f2190774560864b0e1090534113f10e8ee
김민석|Minseok Kim <alstjr7437@naver.com>
2024-06-19 00:48:23 +0900

    feat: 게임 게시판 응답 구현

---
+++ gps/src/payload/response/GameBoardResponse.java
@@ -0,0 +1,25 @@
+package payload.response;
+
+import payload.dto.AppPreviewDTO;
+import payload.dto.GameBoardViewDTO;
+
+import java.util.List;
+import java.util.Date;
+
+public class GameBoardResponse {
+
+    List<GameBoardViewDTO> gameBoardPriviewDTOList;
+
+    public GameBoardResponse(List<GameBoardViewDTO> gameBoardPriviewDTOList) {
+        this.gameBoardPriviewDTOList = gameBoardPriviewDTOList;
+    }
+
+    public List<GameBoardViewDTO> getGameBoardPriviewDTOList() {
+        return gameBoardPriviewDTOList;
+    }
+
+    public void setGameBoardPriviewDTOList(List<GameBoardViewDTO> gameBoardPriviewDTOList) {
+        this.gameBoardPriviewDTOList = gameBoardPriviewDTOList;
+    }
+
+}
